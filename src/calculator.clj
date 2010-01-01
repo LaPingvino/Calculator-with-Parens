4181f519327c19b683ef561b06d6842530e2b1e0
Joop Kiefte <joop@joop-laptop.(none)>
2010-01-01 20:04:04 +0100

    Calculation-pad test ready

--- src/calculator.clj
+++ src/calculator.clj
@@ -2,28 +2,89 @@
 
 (import '(javax.swing JFrame JLabel JTextField JButton)
         '(java.awt.event ActionListener)
-        '(java.awt GridLayout))
+        '(java.awt GridLayout)
+	'(java.math.BigDecimal))
 
-(defn celsius []
-  (let [frame (JFrame. "Celsius Converter")
-        temp-text (JTextField.)
-        celsius-label (JLabel. "Celsius")
+(defn fac [x]
+    (loop [n x f 1]
+        (if (< n 1)
+            f
+            (recur (dec n) (* f n)))))
+
+(defn calcform [from to converter]
+  (let [frame (JFrame. (str "Convert " from " to " to))
+        rootpane (.getRootPane frame)
+	temp-text (JTextField.)
+        from-label (JLabel. from)
         convert-button (JButton. "Convert")
-        fahrenheit-label (JLabel. "Fahrenheit")]
-    (.addActionListener convert-button
+        to-label (JLabel. (str "convert to " to))]
+     (.setDefaultButton rootpane convert-button)
+     (.addActionListener convert-button
       (proxy [ActionListener] []
         (actionPerformed [evt]
-          (let [c (Double/parseDouble (.getText temp-text))]
-            (.setText fahrenheit-label
-               (str (+ 32 (* 1.8 c)) " Fahrenheit"))))))
+          (let [value (. BigDecimal valueOf (Double/parseDouble (.getText temp-text)))]
+            (.setText to-label
+               (str (converter value) to))))))
     (doto frame
       (.setLayout (GridLayout. 2 2 3 3))
       (.add temp-text)
-      (.add celsius-label)
+      (.add from-label)
       (.add convert-button)
-      (.add fahrenheit-label)
+      (.add to-label)
       (.setSize 300 80)
       (.setVisible true))))
 
+(defn converter-fn []
+  (let [frame (JFrame. "Converter")
+        c2f-button (JButton. "Convert °C to °F")
+	f2c-button (JButton. "Convert °F to °C")
+	fac-button (JButton. "Factorial")]
+     (.addActionListener c2f-button
+      (proxy [ActionListener] []
+        (actionPerformed [evt]
+          (calcform "°C" "°F" (fn [c] (+ 32 (* 9/5 c)))))))
+     (.addActionListener f2c-button
+      (proxy [ActionListener] []
+        (actionPerformed [evt]
+          (calcform "°F" "°C" (fn [f] (* (- f 32) 5/9))))))
+     (.addActionListener fac-button
+      (proxy [ActionListener] []
+        (actionPerformed [evt]
+          (calcform "" "!" fac))))
+    (doto frame
+      (.setLayout (GridLayout. 3 1))
+      (.add c2f-button)
+      (.add f2c-button)
+      (.add fac-button)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setSize 300 100)
+      (.setVisible true))))
+
+(defn calculator-base []
+  (let [frame (JFrame. "Calculator")
+        value-1 (JTextField.)
+	operator-selector (JButton. "?")
+	value-2 (JTextField.)
+	parens-1 (JButton. "()")
+	result (JTextField.)
+	parens-2 (JButton. "()")
+	operate-on-1 (JButton. "^")
+	get-result (JButton. "=")
+	operate-on-2 (JButton. "^")]
+    (doto frame
+      (.setLayout (GridLayout. 3 3))
+      (.add value-1)
+      (.add operator-selector)
+      (.add value-2)
+      (.add parens-1)
+      (.add result)
+      (.add parens-2)
+      (.add operate-on-1)
+      (.add get-result)
+      (.add operate-on-2)
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.setSize 150 150)
+      (.setVisible true))))
+
 (defn -main [& cl-args]
-      (celsius))
+      (calculator-base))
