3c60a41e6511a7eac4334dc2d96301500b27b8a0
Joop Kiefte <joop@joop-laptop.(none)>
2010-01-02 16:06:52 +0100

    Principle almost done (done putting everyting in the same code :))

--- src/calculator.clj
+++ src/calculator.clj
@@ -5,22 +5,26 @@
         '(java.awt GridLayout)
         '(java.math.BigDecimal))
 
-(defn calculate []
+(defn calculator [put-value]
   (let [frame (JFrame. "Calculator")
     ; Elements ordered by appearance on the calculator
     value-1 (JTextField.) operator-selector (JButton. "?") value-2 (JTextField.)
     parens-1 (JButton. "( ... )") result (JTextField.) parens-2 (JButton. "( ... )")
     operate-on-1 (JButton. "Put result") get-result (JButton. "=") operate-on-2 (JButton. "Put result")
-    result-value (agent 0)
     operator *]
     
     ; Action listeners to Get Things Done
     (.addActionListener get-result
                         (proxy [ActionListener] []
                                (actionPerformed [evt]
-                                                (if (= (.getText result) "") (.setText result (str (operator (Double/parseDouble (.getText value-1))
-                                                                                                             (Double/parseDouble (.getText value-2)))))
-                                                  (send resultvalue (fn [x] x) (.getText result))))))
+                                                (if (= (.getText result) "")
+                                                    (.setText result (str (operator (bigdec (str "0"(.getText value-1)))
+                                                                                    (bigdec (str "0" (.getText value-2))))))
+                                                  (if put-value (.setText put-value (.getText result)) (System/exit 0))))))
+    (.addActionListener parens-1
+                        (proxy [ActionListener] []
+                               (actionPerformed [evt]
+                                                (calculator value-1))))
     
     ; Putting the elements on the grid
     (doto frame
@@ -30,38 +34,8 @@
           (.add parens-1) (.add get-result) (.add parens-2)
           (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
           (.setSize 450 120)
-          (.setVisible true))
-    (await result-value)))
+          (.setVisible true))))
 
-  (defn calculator-base []
-    (let [frame (JFrame. "Calculator")
-      ; Elements ordered by appearance on the calculator
-      value-1 (JTextField.) operator-selector (JButton. "?") value-2 (JTextField.)
-      parens-1 (JButton. "( ... )") result (JTextField.) parens-2 (JButton. "( ... )")
-      operate-on-1 (JButton. "Put result") get-result (JButton. "=") operate-on-2 (JButton. "Put result")
-      operator *]
-      
-      ; Action listeners to Get Things Done
-      (.addActionListener get-result
-                          (proxy [ActionListener] []
-                                 (actionPerformed [evt]
-                                                  (.setText result (str (operator (Double/parseDouble (.getText value-1))
-                                                                                  (Double/parseDouble (.getText value-2))))))))
-      (.addActionListener parens-1
-                          (proxy [ActionListener] []
-                                 (actionPerformed [evt]
-                                                  (.setText value-1 (calculate)))))
-      
-      ; Putting the elements on the grid
-      (doto frame
-            (.setLayout (GridLayout. 3 3))
-            (.add value-1) (.add operator-selector) (.add value-2)
-            (.add operate-on-1) (.add result) (.add operate-on-2)
-            (.add parens-1) (.add get-result) (.add parens-2)
-            (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-            (.setSize 450 120)
-            (.setVisible true))))
-  
   (defn -main [& cl-args]
-    (calculator-base))
+    (calculator nil))
 
